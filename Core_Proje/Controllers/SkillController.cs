2f17595143c1b9fc29f0661574b44670a3cf4f9e
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-05-06 19:28:01 +0300

    .

---
+++ Core_Proje/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using BusinnesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager =new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var values = skillManager.GetList();
+            return View(values);
+        }
+    }
+}
